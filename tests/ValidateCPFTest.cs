367f93507b5c24769770cec253a6b9a7b67a61a3
Jonathan de Souza <Jonathan.desouza@viavarejo.com.br>
2025-04-25 08:25:52 -0300

    tests: Add tests valid and invalid CPF

---
+++ tests/ValidateCPFTest.cs
@@ -0,0 +1,89 @@
+﻿using signup.Domain.Validate;
+
+namespace tests
+{
+    [Trait("Category", "CPF")]
+    public class ValidateCPFTest
+    {
+        [Fact(DisplayName = "DeveValidarCPFValido1")]
+        public void DeveValidarCPFValido1()
+        {
+            //Arrange
+            const string cpf = "97456321558";
+
+            //Act
+            var act = new ValidateCPF(cpf);
+            bool isValid = act.Validate();
+
+            //Assert
+            Assert.True(isValid);
+        }
+
+        [Fact(DisplayName = "DeveValidarCPFValido2")]
+        public void DeveValidarCPFValido2()
+        {
+            //Arrange
+            const string cpf = "71428793860";
+
+            //Act
+            var act = new ValidateCPF(cpf);
+            bool isValid = act.Validate();
+
+            //Assert
+            Assert.True(isValid);
+        }
+
+        [Fact(DisplayName = "DeveValidarCPFValido3")]
+        public void DeveValidarCPFValido3()
+        {
+            //Arrange
+            const string cpf = "87748248800";
+
+            //Act
+            var act = new ValidateCPF(cpf);
+            bool isValid = act.Validate();
+
+            //Assert
+            Assert.True(isValid);
+        }
+
+        [Fact(DisplayName = "NaoDeveValidarCPF1")]
+        public void NaoDeveValidarCPF1()
+        {
+            // Arrange and Act
+            var act = new ValidateCPF(null);
+            bool isValid = act.Validate();
+
+            //Assert
+            Assert.False(isValid);
+        }
+
+        [Fact(DisplayName = "NaoDeveValidarCPF2")]
+        public void NaoDeveValidarCPF2()
+        {
+            //Arrange
+            const string cpf = "123";
+
+            //Act
+            var act = new ValidateCPF(cpf);
+            bool isValid = act.Validate();
+
+            //Assert
+            Assert.False(isValid);
+        }
+
+        [Fact(DisplayName = "NaoDeveValidarCPF3")]
+        public void NaoDeveValidarCPF3()
+        {
+            //Arrange
+            const string cpf = "11111111111";
+
+            //Act
+            var act = new ValidateCPF(cpf);
+            bool isValid = act.Validate();
+
+            //Assert
+            Assert.False(isValid);
+        }
+    }
+}
